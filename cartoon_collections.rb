789a987fcc28362f92c708e288c0ec9daa1e32b4
Rafael Vargas <vargas718@gmail.com>
2019-01-25 02:06:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ def roll_call_dwarves(dwarf)
   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteers)
+  
 end
 
 def long_planeteer_calls# code an argument here
