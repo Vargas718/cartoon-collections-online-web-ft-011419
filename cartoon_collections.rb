9d55a7f2d094a98abc6b4e2dae230ef822a8fa41
Rafael Vargas <vargas718@gmail.com>
2019-01-25 02:02:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,9 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarf)
+  counter = 1
+  dwarf.each_with_index do |d| 
+    puts "#{counter} #{d}"
+  counter += 1   
+  end
 end
 
 def summon_captain_planet# code an argument here
@@ -14,3 +18,6 @@ def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
+
+"1. Dopey"
+"Dopey"
